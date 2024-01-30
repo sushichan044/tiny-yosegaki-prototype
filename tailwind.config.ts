6d337d9f53af31dd9f606587afdac2cf60c3c5fc
sushi-chaaaan <mail@sushichan.live>
2024-01-31 03:05:40 +0900

    fix: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,21 @@
 import type { Config } from "tailwindcss"
 
+import { withTV } from "tailwind-variants/transformer"
+
+// eslint-disable-next-line no-restricted-imports
+import { contentVisibilityPlugin, pseudoPlugin } from "./plugins/tailwind"
+
 const config: Config = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  plugins: [],
+  plugins: [contentVisibilityPlugin, pseudoPlugin],
   theme: {
     extend: {},
   },
 }
-export default config
+
+// this is needed for use responsive variants
+export default withTV(config)
