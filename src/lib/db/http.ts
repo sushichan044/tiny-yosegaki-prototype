73c193ff9bee1041f345fe251cdc3be813e54b89
sushi-chaaaan <mail@sushichan.live>
2024-01-31 01:26:46 +0900

    Revert "feat: add db"
    This reverts commit abafd393f3632857f6b64b8bc537ac33c946693c.

--- src/lib/db/http.ts
+++ src/lib/db/http.ts
@@ -1,10 +1,11 @@
-import { env } from "@/env"
 import { connect } from "@planetscale/database"
 import { drizzle } from "drizzle-orm/planetscale-serverless"
 
 // create the connection
 const connection = connect({
-  url: env.DATABASE_URL,
+  host: process.env["DATABASE_HOST"],
+  password: process.env["DATABASE_PASSWORD"],
+  username: process.env["DATABASE_USERNAME"],
 })
 
 const db = drizzle(connection)
