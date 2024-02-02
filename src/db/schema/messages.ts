9ea7f5d9430c52d9044e594292750109df635fae
sushi-chaaaan <mail@sushichan.live>
2024-02-03 00:08:24 +0900

    refactor: db table

--- src/db/schema/messages.ts
+++ src/db/schema/messages.ts
@@ -1,14 +1,29 @@
 import { users } from "@/db/schema/users"
-import { boolean, pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core"
+import {
+  boolean,
+  index,
+  pgTable,
+  text,
+  timestamp,
+  uuid,
+} from "drizzle-orm/pg-core"
 
-export const messages = pgTable("messages", {
-  attachmentFileName: text("attachment_file_name"),
-  content: text("content").notNull(),
-  createdAt: timestamp("created_at").defaultNow(),
-  id: uuid("id").defaultRandom().primaryKey(),
-  isAccepted: boolean("is_accepted").notNull().default(false),
-  updatedAt: timestamp("updated_at").defaultNow(),
-  userId: uuid("user_id")
-    .notNull()
-    .references(() => users.userId),
-})
+export const messages = pgTable(
+  "messages",
+  {
+    attachmentFileName: text("attachment_file_name"),
+    content: text("content").notNull(),
+    createdAt: timestamp("created_at").defaultNow(),
+    id: uuid("id").defaultRandom().primaryKey(),
+    isAccepted: boolean("is_accepted").notNull().default(false),
+    updatedAt: timestamp("updated_at").defaultNow(),
+    userId: uuid("user_id")
+      .notNull()
+      .references(() => users.id),
+  },
+  (table) => {
+    return {
+      userIdx: index("user_id_idx").on(table.userId),
+    }
+  },
+)
--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,12 +1,13 @@
 import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core"
+import { createInsertSchema } from "drizzle-zod"
 
 export const users = pgTable("users", {
+  avatarUrl: text("avatar_url").notNull(),
   createdAt: timestamp("created_at").defaultNow(),
   id: uuid("id").defaultRandom().primaryKey(),
-  twitterAvatarUrl: text("twitter_avatar_url").notNull(),
   twitterDisplayName: text("twitter_display_name").notNull(),
   twitterName: text("twitter_name").notNull(),
   updatedAt: timestamp("updated_at").defaultNow(),
-  userId: uuid("user_id").unique().notNull(),
-  userName: text("user_name").notNull(),
 })
+
+export const UserInsertSchema = createInsertSchema(users)
