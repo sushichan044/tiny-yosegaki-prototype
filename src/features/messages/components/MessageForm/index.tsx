fe850ffaacc8cc677c57917d3c86ceb6e33fd387
sushi-chaaaan <mail@sushichan.live>
2024-02-05 02:06:20 +0900

    fix: data handling while fetching message from db

--- src/features/messages/components/MessageForm/index.tsx
+++ src/features/messages/components/MessageForm/index.tsx
@@ -5,6 +5,8 @@ import { getMessageByUser } from "@/features/messages/db"
 import { unstable_cache } from "next/cache"
 
 const getCachedMessageByUser = (userId: string) => {
+  // unstable_cache requires valid json body to cache.
+  // so return object such as {data: MessageSelect | null} instead of raw data.
   return unstable_cache(() => getMessageByUser(userId), [`message-${userId}`], {
     revalidate: 60,
   })()
@@ -15,8 +17,8 @@ type Props = {
 }
 
 const MessageForm: React.FC<Props> = async ({ user }) => {
-  const userMessage = await getCachedMessageByUser(user.id)
-  console.log(userMessage)
+  const { data } = await getCachedMessageByUser(user.id)
+  console.debug("userMessage", data)
 
   return (
     <div>
--- src/features/messages/db.ts
+++ src/features/messages/db.ts
@@ -1,12 +1,29 @@
+import type { MessageSelect } from "@/db/schema/messages"
+
 import { db } from "@/db"
 
-const getMessageByUser = async (userId: string) => {
+const getMessageByUser = async (
+  userId: string,
+): Promise<{ data: MessageSelect | null }> => {
   const message = await db.query.messages.findFirst({
     where: (messages, { eq }) => {
       return eq(messages.userId, userId)
     },
   })
-  return message
+  return {
+    data: message ?? null,
+  }
 }
 
-export { getMessageByUser }
+const getAllMessages = async (): Promise<{ data: MessageSelect[] }> => {
+  const messages = await db.query.messages.findMany({
+    where(message, { eq }) {
+      return eq(message.isAccepted, true)
+    },
+  })
+  return {
+    data: messages,
+  }
+}
+
+export { getAllMessages, getMessageByUser }
