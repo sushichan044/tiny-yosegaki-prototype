d746319a88921845d13f9a4fa715a180f4c19241
sushi-chaaaan <mail@sushichan.live>
2024-02-08 01:29:38 +0900

    chore: add useServerAction

---
+++ src/hooks/useServerAction.ts
@@ -0,0 +1,35 @@
+import { useEffect, useRef, useState, useTransition } from "react"
+
+const useServerAction = <P, R>(
+  action: (_: P) => Promise<R>,
+  onFinished?: (_: R | undefined) => void,
+): [(_: P) => Promise<R | undefined>, boolean] => {
+  const [isPending, startTransition] = useTransition()
+  const [result, setResult] = useState<R>()
+  const [finished, setFinished] = useState(false)
+  const resolver = useRef<(value?: PromiseLike<R> | R) => void>()
+
+  useEffect(() => {
+    if (!finished) return
+
+    if (onFinished) onFinished(result)
+    resolver.current?.(result)
+  }, [result, finished, onFinished])
+
+  const runAction = async (args: P): Promise<R | undefined> => {
+    startTransition(() => {
+      action(args).then((data) => {
+        setResult(data)
+        setFinished(true)
+      })
+    })
+
+    return new Promise((resolve) => {
+      resolver.current = resolve
+    })
+  }
+
+  return [runAction, isPending]
+}
+
+export { useServerAction }
