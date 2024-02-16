be86df92a2616d5e3bcbe85e9ccf5ffb53737cf7
sushi-chaaaan <mail@sushichan.live>
2024-02-16 23:48:52 +0900

    chore: cache utils

--- src/cache.ts
+++ src/cache.ts
@@ -1,4 +1,4 @@
-import { revalidatePath, revalidateTag } from "next/cache"
+import { revalidatePath } from "next/cache"
 
 export const USER_PROFILE_CACHE_TAG = "user-profile"
 export const USER_CREATED_PROJECTS_CACHE_TAG = "user-created-projects"
@@ -8,7 +8,9 @@ export const ALL_ACCEPTED_MESSAGES_CACHE_TAG = "accepted_messages"
 export const ALL_OPENED_PROJECTS_CACHE_TAG = "all-opened-projects"
 
 export const revalidateProject = (projectId: string) => {
-  revalidateTag(ALL_OPENED_PROJECTS_CACHE_TAG)
   revalidatePath(`/project/${projectId}`)
   revalidatePath(`/project/${projectId}/post`)
+  revalidatePath(`/project/${projectId}/edit`)
 }
+
+export const revalidateAll = () => revalidatePath("/", "layout")
