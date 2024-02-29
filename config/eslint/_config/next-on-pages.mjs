a5b83f152aaf5a0a87ba1e8ce2d0e02d87bc4ccb
sushi-chaaaan <mail@sushichan.live>
2024-03-01 02:57:01 +0900

    chore: remove unnused deps

--- config/eslint/_config/next-on-pages.mjs
+++
@@ -1,9 +0,0 @@
-//@ts-check
-
-import { compat } from "../../libs/eslintCompat.mjs"
-import next from "./next.mjs"
-
-/** @type {import("eslint").Linter.FlatConfig[]} */
-const config = [...next, ...compat.extends("plugin:next-on-pages/recommended")]
-
-export default config
--- config/eslint/index.mjs
+++ config/eslint/index.mjs
@@ -1,8 +1,7 @@
 //@ts-check
 
 import nextJs from "./next-js.mjs"
-import nextOnPages from "./next-on-pages.mjs"
 import normal from "./normal.mjs"
 import normalTypescript from "./normal-typescript.mjs"
 
-export { nextJs, nextOnPages, normal, normalTypescript }
+export { nextJs, normal, normalTypescript }
